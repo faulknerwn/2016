37cb3a445f40a450222b2e06d3ddeefcc4eb3811
Wendy Faulkner <wendy.faulkner@gmail.com>
2016-12-10 18:18:52 -0600

    finished

--- emoji/emoji/ViewController.swift
+++ emoji/emoji/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var coolTableView: UITableView!
-    var emojis = ["😃","😎","🤠","🤢","🙌","👀"]
+    var emojis = ["😃","😎","🏖","🏈","🙌","👀"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +36,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let defVC = segue.destination as! DefinitionViewController
+        defVC.emoji = sender as! String
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
