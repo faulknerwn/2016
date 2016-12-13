9ff718f3df6ff52c4048c6e93b8fd39d422cd564
Wendy Faulkner <wendy.faulkner@gmail.com>
2016-12-12 19:44:39 -0600

    first doit done

---
+++ DoIt/DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Wendy Faulkner on 12/11/16.
+//  Copyright © 2016 Leprechaun Skydiving. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
