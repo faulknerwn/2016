59fcc7eb27b89ed8be6c40aee48f00e946e3b683
Wendy Faulkner <wendy.faulkner@gmail.com>
2017-02-14 19:26:49 -0600

    13

---
+++ alamofire/alamofire/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  alamofire
+//
+//  Created by Wendy Faulkner on 2/13/17.
+//  Copyright © 2017 Leprechaun Skydiving. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
