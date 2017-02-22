c0a19ba0e3329417301c737e80bfd20d2b4341d1
Wendy Faulkner <wendy.faulkner@gmail.com>
2017-02-21 21:54:02 -0600

    almost finished reserve repack

--- Reserve Repack Dates/Reserve Repack Dates/AddViewController.swift
+++ Reserve Repack Dates/Reserve Repack Dates/AddViewController.swift
@@ -7,15 +7,128 @@
 //
 
 import UIKit
+import CoreData
+import UserNotifications
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+   
+    @IBOutlet var rigDescription: UITextField!
+    var rig: Rig? = nil
+    let userCalendar = Calendar.current
+    @IBOutlet var datePicker: UIDatePicker!
+    var dueDate = Date()
+    var repackDate = Date()
+    var notificationOneDate = NSDate()
+    var notificationTwoDate = NSDate()
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // Enable or disable features based on authorization.
+        }
+        return true
+    }
+   
+    @IBAction func deleteTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(rig!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        performSegue(withIdentifier: "cancelSegue", sender: nil)
     }
 
+    @IBOutlet var rigDueDate: UILabel!
+    @IBOutlet var addRigButton: UIButton!
+    
+      @IBOutlet var addUpdateDate: UIButton!
+    
+    @IBOutlet var deleteButton: UIButton!
+    
+    @IBAction func addRig(_ sender: Any) {
+        
+        if rig != nil {
+            rig!.title = rigDescription.text
+            rig?.repackDate = repackDate as NSDate
+            rig?.dueDate = dueDate as NSDate
+            
+        } else {
+            
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let rig = Rig(context: context)
+            rig.title = rigDescription.text
+            rig.repackDate = repackDate as NSDate
+
+            rig.dueDate = dueDate as NSDate
+            
+        }
+       
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        performSegue(withIdentifier: "cancelSegue", sender: nil)
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()}
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rigDescription.delegate = self
+        
+        self.getDateAndTime()
+        
+        if rig != nil {
+            
+            rigDescription.text = rig!.title
+            addRigButton.setTitle("Update", for: .normal)
+        } else {
+            deleteButton.isHidden = true
+        }
+
+
+        //assign  to run if changed
+        datePicker.addTarget(self, action: #selector(getDateAndTime), for: .valueChanged)
+        
+        self.getDateAndTime()
+
+    }
+    
+   
+
+   
+
+    func getDateAndTime() {
+        
+        // declare format of date we want to see
+         let dateFormatter = DateFormatter()
+        
+        // DateFormatter.dateFormat(fromTemplate: "YYYY-MM-dd", options: 0, locale: nil)
+        
+        // convert date from datePicker.date to String type
+        
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        repackDate = datePicker.date
+        dueDate = userCalendar.date(byAdding: .day, value: 180, to: repackDate)!  // 180 days
+        
+        
+        notificationOneDate = userCalendar.date(byAdding: .day, value: 166, to: repackDate)! as NSDate
+        notificationTwoDate = userCalendar.date(byAdding: .day, value: 173, to: repackDate)! as NSDate
+        
+        let dueDateString = dateFormatter.string(from: dueDate)
+        
+
+        
+        datePicker.addTarget(self, action: #selector(getDateAndTime), for: .valueChanged)
+        
+        rigDueDate.text = "Rig Due On: \(dueDateString)"
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Reserve Repack Dates/Reserve Repack Dates/ViewController.swift
+++ Reserve Repack Dates/Reserve Repack Dates/ViewController.swift
@@ -7,19 +7,112 @@
 //
 
 import UIKit
+import CoreData
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet var rigList: UITableView!
+    
+    var rigs: [Rig] = []
+    
+    
+    @IBAction func addNew(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        rigList.dataSource = self
+        rigList.delegate = self
+       
+       
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return rigs.count
+        
     }
-
-
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let rig = rigs[indexPath.row]
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        
+        cell.textLabel?.text = rig.title
+        cell.detailTextLabel?.numberOfLines = 0; // 0 means 'no limit'
+        if rig.dueDate != nil {
+            let dueDateString = dateFormatter.string(from: rig.dueDate as! Date)
+        
+            cell.textLabel?.text = rig.title! + "    due on  " + dueDateString
+        
+        }
+        
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rig = rigs[indexPath.row]
+        performSegue(withIdentifier: "addRigSegue", sender: rig)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! AddViewController
+        nextVC.rig = sender as? Rig
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+       if (editingStyle == .delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            let rig = rigs[indexPath.row]
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(rig)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            rigList.reloadData()
+        
+                             }
+    }
+    
+    private func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: IndexPath!) -> Bool {
+        return true
+    }
+    
+  
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        
+        do {
+            
+            rigs = try context.fetch(Rig.fetchRequest())
+            rigList.reloadData()
+            
+        } catch {
+            
+        }
+        
+    }
+    
+    
+    
 }
 
